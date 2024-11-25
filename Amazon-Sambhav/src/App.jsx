dc9e146dbdd52aa3a19601f15aecccb3f6e021ea
Erudite13 <varun.sehgal02@gmail.com>
2024-11-25 22:54:03 +0530

    update file

--- Amazon-Sambhav/src/App.jsx
+++ Amazon-Sambhav/src/App.jsx
@@ -177,11 +177,28 @@ function App() {
               value={selectedCountry}
               onChange={(e) => setSelectedCountry(e.target.value)}
             >
-              <option value="">Select a country</option>
-              {countries.map(country => (
-                <option key={country} value={country}>{country}</option>
-              ))}
+                <option value="">Select a country</option>
+                <option value="India">India</option>
+                <option value="Germany">Germany</option>
+                <option value="USA">USA</option>
+                <option value="Canada">Canada</option>
+                <option value="China">China</option>
+                <option value="Brazil">Brazil</option>
+                <option value="Australia">Australia</option>
+                <option value="UK">UK</option>
+                <option value="Japan">Japan</option>
+                <option value="South Korea">South Korea</option>
+                <option value="Mexico">Mexico</option>
+                <option value="Russia">Russia</option>
+                <option value="France">France</option>
+                <option value="Italy">Italy</option>
+                <option value="South Africa">South Africa</option>
+                <option value="Indonesia">Indonesia</option>
+                <option value="Singapore">Singapore</option>
+                <option value="Vietnam">Vietnam</option>
+                <option value="Thailand">Thailand</option>
             </select>
+
             <button onClick={handleCountryAnalysis}>Analyze Market</button>
             {countryAnalysis && (
               <div className="analysis-box">
